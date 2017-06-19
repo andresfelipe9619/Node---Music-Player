b0f9edad51fc176bde22c86b08fbaab8fc7b6833
andresfelipe9619 <andresfelipe9619@gmail.com>
2017-06-19 15:09:20 -0500

    results

--- controllers/home.js
+++ controllers/home.js
@@ -22,13 +22,13 @@ router.post('/search', function(req, res) {
 
         //use the client for executing the 
 
-        client.query("SELECT nombre_cancion,duracion,ruta FROM cancion WHERE nombre_cancion LIKE $1", ['%' + some + '%'], function(err, result) {
+        client.query("SELECT nombre_cancion,nombre_artista,duracion FROM cancion,artista WHERE  nombre_cancion LIKE $1 and fk_id_artista=id_artista", ['%' + some + '%'], function(err, result) {
             //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
             if (err) {
                 return console.error('error running query', err);
             }
             console.log(result.rows);
-            res.render('user', { usuarios: result.rows });
+            res.render('home', { results: result.rows });
 
         });
     });
